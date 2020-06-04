2ca6cec74e6dccaded9edd80b9aabc4a339d0cd1
Stuart Graham <stuart@stuart-graham.com>
2020-06-04 16:43:16 +0100

    Add regions

--- awsrefs.py
+++ awsrefs.py
@@ -2,7 +2,33 @@
 
 regions = {
 "eu-west-1":"EU (Ireland)",
-"eu-west-2":"EU (London)"
+"eu-west-2":"EU (London)",
+"us-east-1":"US East (N. Virginia)",
+"us-east-2":"US East (Ohio)",
+"us-west-1":"US West (N. California)",
+"us-west-2":"US West (Oregon)",
+"ca-central-1":"Canada (Central)",
+"eu-north-1":"EU (Stockholm)",
+"eu-west-3":"EU (Paris)",
+"eu-west-2":"EU (London)",
+"eu-west-1":"EU (Ireland)",
+"eu-central-1":"EU (Frankfurt)",
+"ap-south-1":"Asia Pacific (Mumbai)",
+"ap-northeast-1":"Asia Pacific (Tokyo)",
+"ap-northeast-2":"Asia Pacific (Seoul)",
+"ap-northeast-3":"Asia Pacific (Osaka-Local)",
+"ap-southeast-1":"Asia Pacific (Singapore)",
+"ap-southeast-2":"Asia Pacific (Sydney)",
+"sa-east-1":"South America (São Paulo)",
+"cn-north-1":"China (Beijing)",
+"cn-northwest-1":"China (Ningxia)",
+"us-gov-east-1":"GovCloud (US-East)",
+"us-gov-west-1":"GovCloud (US-West)",
+"TBD":"Asia Pacific (Jakarta)",
+"TBD":"Bahrain",
+"TBD":"Cape Town",
+"TBD":"Hong Kong SAR",
+"TBD":"Milan"
 }
 
 os = {
@@ -16,3 +42,5 @@ commit = {
 "P":"Partial Upfront",
 "N":"No Upfront"
 }
+
+
