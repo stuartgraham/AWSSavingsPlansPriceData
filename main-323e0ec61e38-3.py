a2ae5114d3e1b2b41c29288dfa19c3353e13faf0
Stuart Graham <stuart@stuart-graham.com>
2020-06-04 22:53:12 +0100

    Add EC2 Savings Plans

--- main-323e0ec61e38-3.py
+++ main-323e0ec61e38-3.py
@@ -1,28 +1,39 @@
 import time
 import requests
-from pprint import pprint
 import awsrefs as aws
 import xlsxwriter
 import collections
 from concurrent.futures import ThreadPoolExecutor
 
 # SETTINGS
-# XLS SETTINGS
+# CSV SETTINGS
+CSV_FILE = 'savings-plans.xlsx'
 FAMILY_PER_TAB = True
 LOOKUP_CODE = False
 
 # PLAN SETTINGS
-PLAN_TYPE = 'compute'
-INSTANCE_FAMILY = []
+PLAN_TYPE = 'ec2'
+INSTANCE_FAMILY = ['m5', 'c5']
 PLAN_LENGTH = [1,3]                           
 PLAN_COMMIT = ['A','N']      
 REGIONS = ['eu-west-1', 'eu-west-2']
 OSES = ['Windows', 'RHEL', 'Linux']
 TENANCY = ['Shared', 'Dedicated']
 
+PLAN_TYPE = 'ec2'
+INSTANCE_FAMILY = ['m5']
+PLAN_LENGTH = [1]                           
+PLAN_COMMIT = ['A']      
+REGIONS = ['eu-west-1']
+OSES = ['Linux']
+TENANCY = ['Shared']
+
 #FIXED URL
 BASE_URL = "https://view-publish.us-west-2.prod.pricing.aws.a2z.com/pricing/2.0/meteredUnitMaps/computesavingsplan/USD/current/"
 MID_URLS = {'compute':"compute-savings-plan-ec2",'ec2':"instance-savings-plan-ec2"}
+#/instance-savings-plan-ec2/1%20year/All%20Upfront/m5,%20c5/EU%20(Ireland)/Windows/Dedicated/index.json?timestamp=1591304347
+#/instance-savings-plan-ec2/1%20year/No%20Upfront/m5/US%20East%20(Ohio)/Linux%20with%20SQL%20Ent/Shared/
+#/compute-savings-plan-ec2/1%20year/No%20Upfront/US%20East%20(Ohio)/Linux%20with%20SQL%20Ent/Shared/
 END_URL = "index.json?timestamp="
 
 
@@ -53,78 +64,86 @@ def construct_urls():
                 
                 for tenancy in TENANCY:
                     if PLAN_TYPE.lower() == 'compute':
-                        URLS.append("{}/{}/{}/{}/{}/{}{}".format(START_URL, term, region, os, tenancy, END_URL, TIMESTAMP))
+                        URLS.append((region, "{}/{}/{}/{}/{}/{}{}".format(START_URL, term, region, os, tenancy, END_URL, TIMESTAMP)))
                     if PLAN_TYPE.lower() == 'ec2':
                         for instance in INSTANCE_FAMILY: 
                             URLS.append("{}/{}/{}/{}/{}/{}/{}{}".format(START_URL, term, instance, region, os, tenancy, END_URL, TIMESTAMP))
 
     print("Working on {} URLS".format(len(URLS)))
+    print(URLS)
     return URLS
 
 
 def get_json(in_url):
     response = requests.get(in_url)
-    if response.status_code != 200:
+    if response.status != 200:
         print("ERROR : Could not connect to {}".format(in_url))
+    print(response.json())
     working_data = response.json()
-    for i in working_data['regions'].values():
-        for k, v in i.items():
-            entry = {}
-            rate = v
-            tenancy = rate['ec2:Tenancy']
-            instance = rate['ec2:InstanceType']
-            spregion = rate['ec2:Location']
-            for k,v in aws.regions.items():
-                if spregion == v:
-                    spregion = k
+    print(working_data)
+    print(type(working_data))
+    working_data = working_data['regions']
+    print(working_data)
+    print(working_data.values()[0])
+    for i in working_data:
+        region = i
 
-            operatingsystem = rate['ec2:OperatingSystem']
-            for k,v in aws.os.items():
-                if operatingsystem == v:
-                    operatingsystem = k
-        
-            commityear = rate['LeaseContractLength']
-            commitamount = rate['PurchaseOption']
-            for k,v in aws.commit.items():
-                if commitamount  == v:
-                    commitamount  = k
+    for k, v in working_data[region].items():
+        entry = {}
+        rate = v
+        tenancy = rate['ec2:Tenancy']
+        instance = rate['ec2:InstanceType']
+        spregion = rate['ec2:Location']
+        for k,v in aws.regions.items():
+            if spregion == v:
+                spregion = k
 
-            commitcode = commityear + commitamount
+        operatingsystem = rate['ec2:OperatingSystem']
+        for k,v in aws.os.items():
+            if operatingsystem == v:
+                operatingsystem = k
+    
+        commityear = rate['LeaseContractLength']
+        commitamount = rate['PurchaseOption']
+        for k,v in aws.commit.items():
+            if commitamount  == v:
+                commitamount  = k
 
-            sprate = rate['price']
-            odrate = rate['ec2:PricePerUnit']
-            spcode = "{}-{}-{}-{}-{}".format(instance, spregion, operatingsystem, tenancy, commitcode)
-            savingper = ((float(odrate)-float(sprate))/float(odrate))*100
+        commitcode = commityear + commitamount
 
-            entry_key = instance+spcode
-            entry = {"instance": instance,
-                    "region": spregion,
-                    "os": operatingsystem,
-                    "tenancy": tenancy,
-                    "commitcode": commitcode,
-                    "odrate": "{:0.4f}".format(float(odrate)),
-                    "sprate": sprate,
-                    "savingper": "{:0.2f}".format(savingper),
-                    }
-            if LOOKUP_CODE == True:
-                entry.update({"spcode" : spcode})
+        sprate = rate['price']
+        odrate = rate['ec2:PricePerUnit']
+        spcode = "{}-{}-{}-{}-{}".format(instance, spregion, operatingsystem, tenancy, commitcode)
+        savingper = ((float(odrate)-float(sprate))/float(odrate))*100
 
-            # Tabulate Excel if True
-            if FAMILY_PER_TAB == True:
-                xls_tab = instance[0]
-            else:
-                xls_tab = 'All'
+        entry_key = instance+spcode
+        entry = {"instance": instance,
+                "region": spregion,
+                "os": operatingsystem,
+                "tenancy": tenancy,
+                "commitcode": commitcode,
+                "odrate": "{:0.4f}".format(float(odrate)),
+                "sprate": sprate,
+                "savingper": "{:0.2f}".format(savingper),
+                 }
+        if LOOKUP_CODE == True:
+            entry.update({"spcode" : spcode})
 
-            if xls_tab not in response_dict:
-                response_dict[xls_tab] = collections.OrderedDict()
-                response_dict[xls_tab][entry_key] = entry
-            else:
-                response_dict[xls_tab][entry_key] = entry
+        # Tabulate Excel if True
+        if FAMILY_PER_TAB == True:
+            xls_tab = instance[0]
+        else:
+            xls_tab = 'All'
+
+        if xls_tab not in response_dict:
+            response_dict[xls_tab] = collections.OrderedDict()
+            response_dict[xls_tab][entry_key] = entry
+        else:
+            response_dict[xls_tab][entry_key] = entry
 
 
 def xlwriter(response_dict):
-    xls_file = "{}-savings-plans.xlsx".format(PLAN_TYPE.lower())
-    workbook = xlsxwriter.Workbook(xls_file)
+    workbook = xlsxwriter.Workbook(CSV_FILE)
 
     bold = workbook.add_format({'bold': True})
     money = workbook.add_format({'num_format': '$#,##0.0000'})
--- readme.md
+++ readme.md
@@ -1,14 +1,15 @@
 # AWS Savings Plans Price Data
-Outputs AWS Savings Plans Data to Microsoft Excel based on region, OS, tenancy and term parameters
+Outputs AWS Savings Plans price to Microsoft Excel file based on region, OS, tenancy and term parameters. Supports EC2 and Compute Savings Plans (EC2 Only)
 
 ### main.py
 Manipulate settings as needed
 
 | Settings | Description | Inputs |
 | :----: | --- | --- |
-| `CSV_FILE` | xlsx file name | `savings-plans.xlsx` |
 | `FAMILY_PER_TAB` | Per Instance Family price tabs | `True/False` |
 | `LOOKUP_CODE` | Concatenated code for VLOOKUP | `True/False` |
+| `PLAN_TYPE` | Compute or EC2 Savings Plans | `compute` or `ec2` |
+| `INSTANCE_FAMILY` | Instance Families | `['m5', 'c5']` |
 | `PLAN_LENGTH` | Savings Plans Years | `[1,3]` |
 | `PLAN_COMMIT` | Commitment [A]ll, [N]o, [P]artial Upfront | `['A','N','P']` |
 | `REGIONS` | AWS Region code | `['eu-west-1', 'eu-west-2']` |
